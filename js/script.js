943977e160aa9246d5946d27c5a6524189ae0bee
MuratTuz <murattuzlali@gmail.com>
2021-09-26 22:19:46 +0200

    navigation menu shrink for normal screens and extra small screens is done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-// Navigation hamburger menu
+// Navigation of the hamburger menu on small and extra small screens
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
